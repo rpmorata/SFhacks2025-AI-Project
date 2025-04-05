7ba9728b2a0eaa7ea829c6bac95680dffcccf0a5
Susana Chang <samanta.chang@utec.edu.pe>
2025-04-04 18:50:24 -0700

    Trying to fork

---
+++ src/components/Burn.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <input
+    type="image"
+  />
+  <button @click="() => { console.log('Clicked!') }">
+    Clasify
+  </button>
+</template>
+
+<style scoped>
+
+</style>
