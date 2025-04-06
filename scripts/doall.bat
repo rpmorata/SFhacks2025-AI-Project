d7d44b31fa5fc232089f342c801b42d5e5686977
Ronin Morata <roninmorata@outlook.com>
2025-04-05 17:42:48 -0700

    fixed?

--- scripts/doall.bat
+++ scripts/doall.bat
@@ -1,9 +1,8 @@
 
 cd ../tools
 
-python .\scrape.py "https://universe.roboflow.com/isha-dvf9n/skin_shade/browse?queryText=-class%3ABrown+-class%3ABlack&pageSize=1000&startingIndex=0&browseQuery=true" --folder white
-python .\scrape.py "https://universe.roboflow.com/isha-dvf9n/skin_shade/browse?queryText=-class%3AWhite&pageSize=1000&startingIndex=0&browseQuery=true" --folder notwhite 
-
+python scrape.py "https://universe.roboflow.com/isha-dvf9n/skin_shade/browse?queryText=-class%3ABrown+-class%3ABlack&pageSize=1000&startingIndex=0&browseQuery=true" --folder white
+python scrape.py "https://universe.roboflow.com/isha-dvf9n/skin_shade/browse?queryText=-class%3AWhite&pageSize=1000&startingIndex=0&browseQuery=true" --folder notwhite
 
 python makedb.py
 python populatedb.py
