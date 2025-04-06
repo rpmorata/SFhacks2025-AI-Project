5d9bfb2be4cb9d891af27d9fa0b724c31687b5da
Ronin Morata <roninmorata@outlook.com>
2025-04-06 00:28:18 -0700

    checkpoint2

--- tools/makedb.py
+++ tools/makedb.py
@@ -7,12 +7,12 @@ conn = sqlite3.connect('../database.db')
 # CREATE TABLE skinType (
 #	id INT primary key,
 #	image VARCHAR(100),
-#	classification ENUM ('Normal', 'Skin Disease', 'Bruses', 'Acne', 'Monkey Pox', 'Burns')
+#	classification ENUM ('normal', 'acne', 'atopicdermatitis', 'bruise', 'chickenpox', 'eczema', 'firstdegburns', 'herpes', 'hives', 'impetigo', 'melanoma', 'monkeypox', 'pimple', 'psoriasis', 'scabies', 'seconddegburns', 'skincancer', 'thirddegburns', 'vitiligo', 'warts')
 # )
 conn.execute('''CREATE TABLE skinType 
 		(id INTEGER PRIMARY KEY AUTOINCREMENT,
 		image_data BLOB,  -- Store the actual binary image data
-        classification TEXT CHECK(classification IN ('Normal', 'Skin Disease', 'Bruses', 'Acne', 'Monkey Pox', 'Burns'))
+        classification TEXT CHECK(classification IN ('normal', 'acne', 'atopicdermatitis', 'bruise', 'chickenpox', 'eczema', 'firstdegburns', 'herpes', 'hives', 'impetigo', 'melanoma', 'monkeypox', 'pimple', 'psoriasis', 'scabies', 'seconddegburns', 'skincancer', 'thirddegburns', 'vitiligo', 'warts'))
 		)''')
 
 conn.close()
