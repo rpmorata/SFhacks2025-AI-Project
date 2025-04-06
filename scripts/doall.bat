9128e0ffd2354a785b9496be262d0d1f86f15ced
Ronin Morata <roninmorata@outlook.com>
2025-04-05 17:30:25 -0700

    Create doall.bat

---
+++ scripts/doall.bat
@@ -0,0 +1,14 @@
+
+cd ../tools
+
+python .\scrape.py "https://universe.roboflow.com/isha-dvf9n/skin_shade/browse?queryText=-class%3ABrown+-class%3ABlack&pageSize=1000&startingIndex=0&browseQuery=true" --folder white
+python .\scrape.py "https://universe.roboflow.com/isha-dvf9n/skin_shade/browse?queryText=-class%3AWhite&pageSize=1000&startingIndex=0&browseQuery=true" --folder notwhite 
+
+
+python makedb.py
+python populatedb.py
+
+
+cd ../src 
+python train.py
+
